5c9ad00d42e2473d86e2699517dbcf2a8cd20d92
Calvin Tran <calvintran1478@protonmail.com>
2024-10-28 03:19:32 -0400

    feat: Display the user's music collection on the library page

--- client/blue-waves/src/pages/LibraryPage.tsx
+++ client/blue-waves/src/pages/LibraryPage.tsx
@@ -1,5 +1,6 @@
-import { createSignal, createResource, Show } from "solid-js";
+import { createSignal, createResource, For, Show, Suspense } from "solid-js";
 import { useNavigate } from "@solidjs/router";
+import { until } from "@solid-primitives/promise";
 import { api } from "../index.tsx";
 import MusicModal from "../components/MusicModal.tsx";
 
@@ -17,10 +18,36 @@ const LibraryPage = () => {
         }
     });
 
+    const [musicEntries] = createResource(async () => {
+        // Wait until access token is fetched
+        await until(() => token() !== undefined)
+
+        // Get music entries
+        const musicResponse = await api.get("users/music", {
+            headers: {
+                "Authorization": `Bearer ${token()}`
+            }
+        }).json<{"music": {"title": string, "artist": string}[]}>();
+
+        return musicResponse["music"]
+    });
+
     return (
         <div>
             <div>
-                <button class="rounded border-2" onClick={() => setShowMusicModal(true)}>Add Music</button>
+                <h1 class="text-2xl font-semibold my-4">Your Library</h1>
+                <button class="rounded border-2 my-2" onClick={() => setShowMusicModal(true)}>Add Music</button>
+                <hr class="border my-2" />
+                <Suspense>
+                    <For each={musicEntries()}>
+                        {(musicEntry) => (
+                            <div class="h-16 w-auto my-2 border-2">
+                                <h2 class="text-lg font-semibold">{musicEntry["title"]}</h2>
+                                <h3 class="text-lg">{musicEntry["artist"]}</h3>
+                            </div>
+                        )}
+                    </For>
+                </Suspense>
             </div>
             <Show when={showMusicModal()}>
                 <div class="flex justify-center items-center h-screen w-screen fixed inset-0 bg-black/50">
