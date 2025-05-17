1d7c0d5e882b828c50fb67cd79610297ed2463c0
Calvin Tran <calvintran1478@protonmail.com>
2025-05-17 03:49:49 -0400

    fix: Limit bytes read on register and login

--- server/src/validators/user_validator.cr
+++ server/src/validators/user_validator.cr
@@ -5,6 +5,14 @@ require "../schemas/user_schemas"
 module Validators::UserValidator
   include Schemas::UserSchemas
 
+  MAX_EMAIL_LENGTH = 60
+
+  MIN_PASSWORD_LENGTH = 8
+  MAX_PASSWORD_LENGTH = 71
+
+  MAX_FIRST_NAME_LENGTH = 50
+  MAX_LAST_NAME_LENGTH = 50
+
   # Validates POST requests sent to api/v1/users when registering an account.
   #
   # Returns the parsed request body as a RegisterRequest struct if validation is successful, and nil otherwise.
@@ -20,65 +28,101 @@ module Validators::UserValidator
   # end
   # ```
   def validate_register_request(context : HTTP::Server::Context) : (RegisterRequest | Nil)
-    # Parse request body
+    # Get request body
     if context.request.body.nil?
       context.response.status = HTTP::Status::BAD_REQUEST
       return
     end
-
     request_body = context.request.body.as(IO)
-    email = request_body.gets
-    password = request_body.gets
-    first_name = request_body.gets
-    last_name = request_body.gets
 
-    if email.nil? || password.nil? || first_name.nil? || last_name.nil?
+    # Parse and validate email
+    email = request_body.gets('\n', MAX_EMAIL_LENGTH + 1, chomp: true)
+    if email.nil?
       context.response.status = HTTP::Status::BAD_REQUEST
+      context.response.output << "Email required"
       return
     end
 
-    # Check email is valid
     if !Valid.email?(email)
       context.response.status = HTTP::Status::BAD_REQUEST
       context.response.output << "Invalid email"
       return
     end
 
-    # Check that the entered first and last names are non-empty
-    if first_name.empty? || last_name.empty?
+    # Parse and validate password
+    password = request_body.gets('\n', MAX_PASSWORD_LENGTH + 1, chomp: true)
+    if password.nil?
       context.response.status = HTTP::Status::BAD_REQUEST
-      context.response.output << "First and last names cannot be empty"
+      context.response.output << "Password required"
       return
     end
 
-    # Check that the entered first and last names consist of alphabetical characters
-    first_name.each_char do |ch|
-      if !ch.ascii_letter?
-        context.response.status = HTTP::Status::BAD_REQUEST
-        context.response.output << "First and last names must consist of alphabetical characters"
-        return
-      end
-    end
-
-    last_name.each_char do |ch|
-      if !ch.ascii_letter?
-        context.response.status = HTTP::Status::BAD_REQUEST
-        context.response.output << "First and last names must consist of alphabetical characters"
-        return
-      end
-    end
-
-    # Check password meets length requirements
-    if password.size < 8
+    if password.size < MIN_PASSWORD_LENGTH
       context.response.status = HTTP::Status::BAD_REQUEST
       context.response.output << "Password must be at least 8 characters"
       return
     end
 
-    if password.size > 71
+    if password.size > MAX_PASSWORD_LENGTH
       context.response.status = HTTP::Status::BAD_REQUEST
       context.response.output << "Password cannot exceed 71 characters"
       return
+    end
+
+    # Parse and validate first name
+    first_name = request_body.gets('\n', MAX_FIRST_NAME_LENGTH + 1, chomp: true)
+    if first_name.nil?
+      context.response.status = HTTP::Status::BAD_REQUEST
+      context.response.output << "First name required"
+      return
+    end
+
+    if first_name.empty?
+      context.response.status = HTTP::Status::BAD_REQUEST
+      context.response.output << "First name cannot be empty"
+      return
+    end
+
+    if first_name.size > MAX_FIRST_NAME_LENGTH
+      context.response.status = HTTP::Status::BAD_REQUEST
+      context.response.output << "First name cannot exceed 50 characters"
+      return
+    end
+
+    first_name.each_char do |ch|
+      if !ch.ascii_letter?
+        context.response.status = HTTP::Status::BAD_REQUEST
+        context.response.output << "First name must consist of alphabetical characters"
+        return
+      end
+    end
+
+    # Parse and validate last name
+    last_name = request_body.gets('\n', MAX_LAST_NAME_LENGTH + 1, chomp: true)
+    if last_name.nil?
+      context.response.status = HTTP::Status::BAD_REQUEST
+      context.response.output << "Last name required"
+      return
+    end
+
+    if last_name.empty?
+      context.response.status = HTTP::Status::BAD_REQUEST
+      context.response.output << "Last name cannot be empty"
+      return
+    end
+
+    if last_name.size > MAX_LAST_NAME_LENGTH
+      context.response.status = HTTP::Status::BAD_REQUEST
+      context.response.output << "Last name cannot exceed 50 characters"
+      return
+    end
+
+    last_name.each_char do |ch|
+      if !ch.ascii_letter?
+        context.response.status = HTTP::Status::BAD_REQUEST
+        context.response.output << "Last name must consist of alphabetical characters"
+        return
+      end
     end
 
     RegisterRequest.new(email, password, first_name, last_name)
@@ -99,18 +143,38 @@ module Validators::UserValidator
   # end
   # ```
   def validate_login_request(context : HTTP::Server::Context) : (LoginRequest | Nil)
-    # Parse request body
+    # Get request body
     if context.request.body.nil?
       context.response.status = HTTP::Status::BAD_REQUEST
       return
     end
-
     request_body = context.request.body.as(IO)
-    email = request_body.gets
-    password = request_body.gets
 
-    if email.nil? || password.nil?
+    # Parse and validate email
+    email = request_body.gets('\n', MAX_EMAIL_LENGTH + 1, chomp: true)
+    if email.nil?
       context.response.status = HTTP::Status::BAD_REQUEST
+      context.response.output << "Email required"
+      return
+    end
+
+    if !Valid.email?(email)
+      context.response.status = HTTP::Status::BAD_REQUEST
+      context.response.output << "Invalid email"
+      return
+    end
+
+    # Parse and validate password
+    password = request_body.gets('\n', MAX_PASSWORD_LENGTH + 1, chomp: true)
+    if password.nil?
+      context.response.status = HTTP::Status::BAD_REQUEST
+      context.response.output << "Password required"
+      return
+    end
+
+    if password.size > MAX_PASSWORD_LENGTH
+      context.response.status = HTTP::Status::BAD_REQUEST
+      context.response.output << "Password cannot exceed 71 characters"
       return
     end
 
