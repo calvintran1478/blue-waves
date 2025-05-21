64fc1890a5929a62991805cc880afaeea2f729ff
Calvin Tran <calvintran1478@protonmail.com>
2025-05-21 03:51:21 -0400

    docs: Add documentation for auth middleware

--- server/src/middleware/auth_middleware.cr
+++ server/src/middleware/auth_middleware.cr
@@ -2,10 +2,21 @@ require "http/server"
 require "../utils/str"
 require "../utils/token"
 
+# Middleware for authenticating user requests made to a protected endpoint
 class Middleware::AuthMiddleware
   def initialize(@auth_db : Redis::PooledClient, @API_SECRET : String)
   end
 
+  # Retreives user id from the given HTTP server context.
+  #
+  # The user id is written to the provided buffer, which must be large enough to
+  # store 49 bytes. If authentication fails for any reason this function returns
+  # nil and writes a status code of 401 to the response header.
+  #
+  # ```
+  # user_id_buffer = uninitialized UInt8[49]
+  # user_id = @auth_middleware.get_user(context, user_id_buffer.to_unsafe)
+  # ```
   def get_user(context : HTTP::Server::Context, user_id_buffer : UInt8*) : (String | Nil)
     # Check that the authorization header is included
     auth_header = context.request.headers["Authorization"]?
