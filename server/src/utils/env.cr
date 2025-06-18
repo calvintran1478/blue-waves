e71bb432a2c79c3148beb46c035b1e135ef19544
Calvin Tran <calvintran1478@protonmail.com>
2025-06-18 15:36:03 -0400

    fix: Check if .env file is present before starting the server

--- server/src/utils/env.cr
+++ server/src/utils/env.cr
@@ -10,8 +10,15 @@ module Utils::Env
   # Utils::Env.load_env()
   # ```
   def load_env()
+    # Check environment file exists
+    if !File.exists?(".env")
+      puts "Error: Missing .env file"
+      exit 1
+    end
+
+    # Read environment variables
     File.each_line(".env") do |line|
-      equals_index = line.index("=")
+      equals_index = line.index('=')
       if !equals_index.nil?
         ENV[line[...equals_index]] = line[(equals_index + 1)..]
       end
