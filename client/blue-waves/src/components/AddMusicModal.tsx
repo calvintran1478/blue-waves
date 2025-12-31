604fc182de264e4964b9df5eebc30ec3fd296cde
Calvin Tran <calvintran1478@protonmail.com>
2025-12-31 01:18:42 -0500

    fix: Associate file input labels to the correct inputs

--- client/blue-waves/src/components/AddMusicModal.tsx
+++ client/blue-waves/src/components/AddMusicModal.tsx
@@ -71,11 +71,11 @@ const AddMusicModal = (props: { closeCallback: () => void, musicEntries: Resourc
                     <input id="artist" class="border-2 m-2 w-60 h-8" onChange={(event) => {artist = event.target.value}} maxlength={100} required/>
                 </div>
                 <div class="flex justify-center m-3">
-                    <label for="artist" class="text-lg m-2">Music</label>
+                    <label for="musicfile" class="text-lg m-2">Music</label>
                     <input ref={musicInput} type="file" id="musicFile" class="w-60 m-2" required/>
                 </div>
                 <div class="flex justify-center m-3 mr-10">
-                    <label for="artist" class="text-lg m-2">Cover Art</label>
+                    <label for="artFile" class="text-lg m-2">Cover Art</label>
                     <input ref={artInput} type="file" id="artFile" class="w-60 m-2"/>
                 </div>
                 <button class="inline-flex items-center border-2 rounded p-3 bg-neutral-400 mt-5" disabled={addMusicLoading()}>
